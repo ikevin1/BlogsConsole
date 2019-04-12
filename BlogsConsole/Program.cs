a331daead4648f1288bd3bd65c5fa7648d23aded
ikevin <ikevin@my.wctc.edu>
2019-04-11 20:25:36 -0700

    updated posts
    Added Display Blogs

--- BlogsConsole/Program.cs
+++ BlogsConsole/Program.cs
@@ -11,37 +11,77 @@ namespace BlogsConsole
         public static void Main(string[] args)
         {
             logger.Info("Program started");
-            try
+            string choice = "";
+            do
             {
-
-                // Create and save a new Blog
-                Console.Write("Enter a name for a new Blog: ");
-                var name = Console.ReadLine();
-
-                var blog = new Blog { Name = name };
-
-                var db = new BloggingContext();
-                db.AddBlog(blog);
-                logger.Info("Blog added - {name}", name);
-
-                // Display all Blogs from the database
-                var query = db.Blogs.OrderBy(b => b.Name);
-
-                Console.WriteLine("All blogs in the database:");
-                foreach (var item in query)
+                try
                 {
-                    Console.WriteLine(item.Name);
-                }
-            }
-            catch (Exception ex)
-            {
-                logger.Error(ex.Message);
-            }
+                    DisplayMenu();
+                    var dbContext = new BloggingContext();
 
+                    logger.Info("User choice: {Choice}", choice);
+
+                    if (choice == "3") //add post
+                    {
+                        Console.Write("Enter a Blog name for a new Post: ");
+                        var Blogname = Console.ReadLine();
+
+                        Blog MyBlog = dbContext.Blogs.Where(id => id.Name == Blogname).FirstOrDefault();
+
+                        Console.WriteLine("Enter Title: ");
+                        var myTitle = Console.ReadLine();
+                        Console.WriteLine("\nEnter Content: ");
+                        var myContent = Console.ReadLine();
+
+                        //add post
+                        Post post = new Post { Title = myTitle, Content = myContent, BlogId = MyBlog.BlogId };
+                        dbContext.AddPost(post);
+                    }
+
+                    else if (choice == "2") //add blog
+                    {
+
+                        Console.Write("Enter a name for a new Blog: ");
+                        var name = Console.ReadLine();
+
+                        var blog = new Blog { Name = name };
+                        dbContext.AddBlog(blog);
+                    }
+                    else if (choice == "1") //display all blogs
+                    {
+                        Console.WriteLine("All blogs in the database:");
+
+                        var query = dbContext.Blogs.OrderBy(b => b.Name);
+                        foreach (var item in query)
+                        {
+                            Console.WriteLine("BlogID: {0}\nBlogName: {1}", item.BlogId, item.Name);
+                        }
+                    }
+
+                }
+
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex);
+                    Console.ReadLine();
+
+                    logger.Error(ex.Message);
+                    Console.ReadLine();
+                }
+            } while (choice == "1" || choice == "2" || choice == "3");
             Console.WriteLine("Press enter to quit");
             string x = Console.ReadLine();
 
             logger.Info("Program ended");
+            void DisplayMenu()
+            {
+                Console.WriteLine("1) Display All Blogs");
+                Console.WriteLine("2) Add Blogs");
+                Console.WriteLine("3) Create Post");
+                Console.WriteLine("Enter to quit");
+                // input selection
+                choice = Console.ReadLine();
+            }
         }
     }
 }
